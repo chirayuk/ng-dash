30a1f39e623feffbb41c2113948013c54f2427ea
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-06-09 18:26:59 -0700

    WIP: create entries for individual runs and repeated fields for searches

--- server/models.py
+++ server/models.py
@@ -1,73 +1,112 @@
 # -*- coding: utf-8 -*-
 
-"""Datastore NDB models for metrics per test run.
-
-Our datastore models are specified in python by subclassing "ndb.Model".  We'll
-define the message model thats transferred between the client and server as our
-canonical model and then define the datastore model in terms of it.
-"""
-
 from protorpc import messages
+from protorpc.messages import (
+    IntegerField, StringField, MessageField, Variant)
 
 import google.appengine.ext.ndb.msgprop
+from google.appengine.ext.ndb import (
+    DateTimeProperty, StringProperty, StructuredProperty, KeyProperty
 ndb = google.appengine.ext.ndb
 
 
 package = "com.appspot.ng-dash"
 
 
-class Data(messages.Message):
-  name = messages.StringField(1)
-  description = messages.StringField(2)
-  dimensionsJson = messages.StringField(3)
-  metricsJson = messages.StringField(4)
-  children = messages.MessageField('Data', 5, repeated=True)
+
+class TestData(messages.Message):
+  name = StringField(1)
+  description = StringField(2)
+  dimensions_json = StringField(3)
+  metrics_json = StringField(4)
+  parent_id = StringField(5)
+  children = MessageField('TestData', 6, repeated=True)
+  dimensions_merged_json = StringField(7)
+  dimensions_flattened_json = StringField(8)
+
+
+class NvModel(ndb.Model):
+  name = ndb.StringProperty()
+  value = ndb.StringProperty()
+
+
+class TestDataModel(ndb.Model):
+  name = StringProperty()
+  description = StringProperty()
+  dimensions_json = StringProperty()
+  metrics_json = StringProperty();
+  parent = KeyProperty(kind='TestDataModel')
+  children = KeyProperty(kind='TestDataModel', repeated=true)
+  # For searching
+  dimensions = StructuredProperty(NvModel, repeated=True)
+  dimensions_merged = StructuredProperty(NvModel, repeated=True)
+  dimensions_flattened = StructuredProperty(NvModel, repeated=True)
+  metrics = StructuredProperty(NvModel, repeated=True)
 
 
 class RunInfo(messages.Message):
-  id = messages.StringField(1)
-  commitSha = messages.StringField(2, required=True)
-  createdTimestamp = messages.IntegerField(
-      3, variant=messages.Variant.INT32)
-  name = messages.StringField(4)
-  description = messages.StringField(5)
-  # TODO(chirayu): resolve creatorIdentity from the OAUTH identity of the
-  # poster.
-  creatorIdentity = messages.StringField(6)
-  data = messages.MessageField(Data, 7)
+  id = StringField(1)
+  commit_sha = StringField(2, required=True)
+  tree_sha = StringField(3, required=True)
+  creation_timestamp = messages.IntegerField(4, variant=Variant.INT32)
+  name = StringField(5)
+  description = StringField(6)
+  creator_email = StringField(7)
+  test_data = MessageField(TestData, 8)
 
 
 class RunInfoModel(ndb.Model):
-  msg = ndb.msgprop.MessageProperty(
-      RunInfo,
-      indexed_fields=[
-          "id",
-          "commitSha",
-          "name",
-          ]
-      )
+  id = StringProperty()
+  commit_sha = StringProperty()
+  tree_sha = StringProperty()
+  creation_time = DateTimeProperty(auto_now_add=True)
+  name = StringProperty()
+  description = StringProperty()
+  # stable user.user_id()
+  creator_id = StringProperty()
+  creator_email = StringProperty()
+  test_data = KeyProperty(kind=TestDataModel)
+
 
   @classmethod
   def Get(cls, id):
     result = ndb.Key(cls, int(id)).get()
-    result.msg.id = str(result.key.id())
     return result
 
   @classmethod
-  def _fixup_ids(self, results):
-    for result in results:
-      result.msg.id = str(result.key.id())
-    return results
+  def _message_from_model(cls, model):
+    msg = RunInfo()
+    msg.id = str(model.key.id())
+    msg.commit_sha = model.commit_sha
+    msg.tree_sha = model.tree_sha
+    msg.creation_timestamp = int(calendar.timegm(model.creation_time.utctimetuple()))
+    msg.name = model.name
+    msg.description = model.description
+    msg.creator_email = model.creator_email
+    msg.test_data = model.test_data._message_from_model()
+
+  @classmethod
+  def _model_from_message(cls, msg):
+    model = RunInfoModel()
+    # The following fields will NOT be copied:
+    # id, creation_time, creator_id, creator_email
+    # test_data
+    model.commit_sha = msg.commit_sha
+    model.tree_sha = msg.tree_sha
+    model.name = msg.name
+    model.description = msg.description
+    return model
+
 
   @classmethod
   def QueryAll(cls, limit=20):
-    return cls._fixup_ids(cls.query().fetch(limit))
+    return cls.query().fetch(limit)
 
   @classmethod
   def QueryBySha(cls, sha, limit=20):
-    return cls._fixup_ids(cls.query(cls.msg.commitSha == sha).fetch(limit))
+    return cls.query(cls.msg.commit_sha == sha).fetch(limit)
 
 
 # For listing all all RunInfo's per commit SHA.
 class RunInfoCollection(messages.Message):
-  items = messages.MessageField(RunInfo, 1, repeated=True)
+  items = MessageField(RunInfo, 1, repeated=True)
